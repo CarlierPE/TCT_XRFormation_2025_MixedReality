1a636983680d949c5b889f08a6152507e0260bdd
Carlier PE <Carlier.PE@gmail.com>
2025-06-18 13:52:13 +0200

    singleton provider for guide
    avoiding the "getinstance" burden that making guide itself a singleton would create

---
+++ UnityProject/FireEscapeSimulator/Assets/_MainProject/Scripts/Guide.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Guide : MonoBehaviour
+{
+    /*
+     * will contain public methods to control the guide animations etc
+     * maybe a "moveto" or something? we will see how intelligence is balanced between the guide itself
+     * and the path manager 
+     * 
+     */
+}
