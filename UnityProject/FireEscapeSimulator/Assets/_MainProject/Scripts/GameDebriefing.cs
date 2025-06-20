e8e6c31c2459723816bc45e6ca4ec52bb3e96408
sebastien verhaegen <sebastien.verhaegen@outlook.com>
2025-06-20 21:50:42 +0200

    Add Save Game

---
+++ UnityProject/FireEscapeSimulator/Assets/_MainProject/Scripts/GameDebriefing.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class GameDebriefing
+{
+    public float startGame;
+    public int scoreEnd;
+    public List<ScoreLog> scoreLogs;
+}
