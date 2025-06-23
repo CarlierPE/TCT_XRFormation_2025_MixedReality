7c8a88edac3cb4dba9543e00a3ebab0029cdbc56
Carlier PE <Carlier.PE@gmail.com>
2025-06-23 13:18:31 +0200

    Basic injection for a couple states
    Will help testing the SSM system

--- UnityProject/FireEscapeSimulator/Assets/_MainProject/Scripts/SSMStates/CalibratedState.cs
+++ UnityProject/FireEscapeSimulator/Assets/_MainProject/Scripts/SSMStates/CalibratedState.cs
@@ -1,12 +1,29 @@
+using System;
 using FireSim.SSM;
 using UnityEngine;
 
 public class CalibratedState : GameState
 {
     public override eGameStateID ID => eGameStateID.Calibrated;
-
+    private GameObject _calibrationConfirmationObject;
     public override bool CanTransitionTo(eGameStateID nextState)
     {
         return nextState == eGameStateID.Uncalibrated || nextState == eGameStateID.BeforeTutorial;
+    }
+
+    public CalibratedState(GameObject calibrationConfirmationObject)
+    {
+        if(calibrationConfirmationObject == null) throw new ArgumentNullException(nameof(calibrationConfirmationObject));
+        _calibrationConfirmationObject = calibrationConfirmationObject;
+    }
+
+    public override void OnEnter()
+    {
+        _calibrationConfirmationObject.SetActive(true);
+    }
+
+    public override void OnExit()
+    {
+        _calibrationConfirmationObject.SetActive(false);
     }
 }
--- UnityProject/FireEscapeSimulator/Assets/_MainProject/Scripts/SSMStates/UncalibratedState.cs
+++ UnityProject/FireEscapeSimulator/Assets/_MainProject/Scripts/SSMStates/UncalibratedState.cs
@@ -1,12 +1,30 @@
+using System;
 using FireSim.SSM;
 using UnityEngine;
 
 public class UncalibratedState : GameState
 {
     public override eGameStateID ID => eGameStateID.Uncalibrated;
-
+    private GameObject _calibrationObject;
     public override bool CanTransitionTo(eGameStateID nextState)
     {
         return nextState == eGameStateID.Calibrated;
+    }
+
+    public override void OnEnter()
+    {
+        _calibrationObject.SetActive(true);
+    }
+
+    public override void OnExit()
+    {
+        _calibrationObject.SetActive(false);
+    }
+
+    public UncalibratedState(GameObject calibrationObject)
+    {
+        if(calibrationObject == null) throw new ArgumentNullException(nameof(calibrationObject));
+
+        _calibrationObject = calibrationObject;
     }
 }
