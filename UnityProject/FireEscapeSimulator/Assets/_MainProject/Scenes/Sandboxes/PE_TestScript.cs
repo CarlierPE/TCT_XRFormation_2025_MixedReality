59e9c1337523d70974949b57b03a35b4eec1ae31
Carlier PE <Carlier.PE@gmail.com>
2025-06-25 13:55:39 +0200

    Preparation of the tutorial steps scripts

--- UnityProject/FireEscapeSimulator/Assets/_MainProject/Scenes/Sandboxes/PE_TestScript.cs
+++ UnityProject/FireEscapeSimulator/Assets/_MainProject/Scenes/Sandboxes/PE_TestScript.cs
@@ -4,19 +4,6 @@ using UnityEngine.InputSystem;
 
 public class PE_TestScript : TriggerableByPlayer
 {
-    void osef()
-    {
-        TutorialStep tutorialStep = new SphereMovement();
-
-        osef2(tutorialStep);
-
-    }
-
-
-    void osef2(TutorialStep step)
-    {
-        step.StartStep();
-    }
     //for debugging purposes - fires the event every 5 sec
     void Update()
     {
