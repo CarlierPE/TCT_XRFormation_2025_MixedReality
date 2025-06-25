bbb35056220b5b8e0aa2da3d0a3a1c187e764c97
sebastien verhaegen <sebastien.verhaegen@outlook.com>
2025-06-25 15:06:23 +0200

    Modifed Save and Score for Save In the folder

--- UnityProject/FireEscapeSimulator/Assets/_MainProject/Scenes/Sandboxes/Seb_TestSave.cs
+++ UnityProject/FireEscapeSimulator/Assets/_MainProject/Scenes/Sandboxes/Seb_TestSave.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 
 public class Seb_TestSave : MonoBehaviour
 {
-    private string _rootPath;
+    private const string _extension = ".json";
+    private const string _prefix = "Player_";
+
+    private string _rootPathSearch;
     private string _folderName;
     private string _fileName;
     private string _fileNumber;
-    private string _fileExtension = ".json";
-    private string _fullFilePath;
-    private string _rootFullPath;
-    private string _saveDocument;
+    private string _PathCreatedFolder;
+    private string _rootPathToSave;
 
     private int _numFile;
     private int _fileCount;
-    private int _saveCount;
     private string _currentDate;
 
     private List<GameDebriefing> _gameDebriefings;
@@ -26,20 +26,19 @@ public class Seb_TestSave : MonoBehaviour
         _gameDebriefings = new List<GameDebriefing>(); // Initialize the list to hold game debriefings
         _currentDate = DateTime.Now.ToString("yyyy-MM-dd");
         _folderName = _currentDate.Replace("-", "_"); // Format the date as yyyy_mm_dd
-        _rootPath = Application.persistentDataPath;
-        _fileName = "Player_";
+        _rootPathSearch = Application.persistentDataPath;
         _fileNumber = "01"; // Start with file number 01
-        _fullFilePath = $"{_rootPath}/{_folderName}";
+        _fileName = $"{_prefix}{_fileNumber}{_extension}";
+        _PathCreatedFolder = $"{_rootPathSearch}/{_folderName}";
         CreateFolderIfNeeded();
-        _saveDocument = $"{_fileName}{_fileNumber}{_fileExtension}";
-        _rootFullPath = $"{_fullFilePath}/{_saveDocument}";
-        if (File.Exists(_rootFullPath))
+        _rootPathToSave = $"{_PathCreatedFolder}/{_fileName}";
+        if (File.Exists(_rootPathToSave))
         {
             ReadOnFolder();
 
             if(_gameDebriefings == null || _gameDebriefings.Count == 0)
             {
-                _numFile = 1; // Reset to 1 if no files exist
+                _numFile = 2; // Reset to 1 if no files exist
             }
             else
             {
@@ -55,57 +54,46 @@ public class Seb_TestSave : MonoBehaviour
 
     private void CreateFolderIfNeeded()
     {
-        if(!ResearchToFolder())
-            Directory.CreateDirectory(_fullFilePath); // Create the directory if it doesn't exist
+        if(!Directory.Exists(_PathCreatedFolder))
+            Directory.CreateDirectory(_PathCreatedFolder); // Create the directory if it doesn't exist
     }
 
     private string ChangedName()
     {
         _fileNumber = _numFile.ToString("D2"); // Format the file number with leading zeros
-        _saveDocument = $"{_fileName}{_fileNumber}{_fileExtension}"; // Create the new file name
-        _rootFullPath = $"{_rootPath}/{_folderName}/{_saveDocument}"; // Update the full file path
-        return $"File name changed to: {_saveDocument}";
-    }
-
-    private bool ResearchToFolder()
-    {
-        return Directory.Exists(_fullFilePath); // Check if the directory exists
-    }
-
-    private bool ResearchToFile()
-    {
-        return File.Exists(_rootFullPath); // Check if the file exists
+        _fileName = $"{_prefix}{_fileNumber}{_extension}"; // Create the new file name
+        _rootPathToSave = $"{_PathCreatedFolder}/{_fileName}"; // Update the full file path
+        _numFile++;
+        return $"File name changed to: {_fileName}";
     }
 
     public void SaveDocument(GameDebriefing saveFile)
     {
-        if(!ResearchToFile())
+        if(File.Exists(_rootPathToSave))
         {
             ChangedName(); // Change the file name if it doesn't exist
         }
 
         string json = JsonUtility.ToJson(saveFile, true); // Convert the GameDebriefing object to JSON
         
-        File.WriteAllText(_rootFullPath, json); // Write the JSON to the file
+        File.WriteAllText(_rootPathToSave, json); // Write the JSON to the file
     }
 
     public List<GameDebriefing> ReadtoFile()
     {
-        if(_rootFullPath == null)
+        if(_PathCreatedFolder == null)
             InitBased();
 
-        string[] fileJson = Directory.GetFiles(_fullFilePath);
+        string[] fileJson = Directory.GetFiles(_PathCreatedFolder);
 
         if(fileJson.Length == 0 || fileJson == null)
             return new List<GameDebriefing>();
 
-        GameDebriefing readSaved = new GameDebriefing();
-
         foreach (var file in fileJson)
         {
             string contenu = File.ReadAllText(file);
-            readSaved = JsonUtility.FromJson<GameDebriefing>(contenu);
-            _gameDebriefings.Add(readSaved);
+
+            _gameDebriefings.Add(JsonUtility.FromJson<GameDebriefing>(contenu));
         }
 
         return _gameDebriefings;
@@ -113,18 +101,16 @@ public class Seb_TestSave : MonoBehaviour
 
     private void ReadOnFolder()
     {
-        string[] fileJson = Directory.GetFiles(_fullFilePath);
+        string[] fileJson = Directory.GetFiles(_PathCreatedFolder);
 
         if (fileJson.Length == 0 || fileJson == null)
             _gameDebriefings = null;
 
-        GameDebriefing readSaved = new GameDebriefing();
-
         foreach (var file in fileJson)
         {
             string contenu = File.ReadAllText(file);
-            readSaved = JsonUtility.FromJson<GameDebriefing>(contenu);
-            _gameDebriefings.Add(readSaved);
+
+            _gameDebriefings.Add(JsonUtility.FromJson<GameDebriefing>(contenu));
         }
     }
 }
