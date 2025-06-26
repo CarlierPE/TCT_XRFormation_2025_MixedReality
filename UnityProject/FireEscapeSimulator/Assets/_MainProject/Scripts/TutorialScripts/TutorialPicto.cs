6eb7b59f64022e2f6e174950101771dc4f91cb6f
Carlier PE <Carlier.PE@gmail.com>
2025-06-26 11:32:46 +0200

    picto class definition

--- UnityProject/FireEscapeSimulator/Assets/_MainProject/Scripts/TutorialScripts/TutorialPicto.cs
+++ UnityProject/FireEscapeSimulator/Assets/_MainProject/Scripts/TutorialScripts/TutorialPicto.cs
@@ -1,16 +1,9 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TutorialPicto : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //TODO - canvas, boutons etc
+    public UnityEvent OnUserConfirmation;
+    public UnityEvent OnUserCancellation;
 }
