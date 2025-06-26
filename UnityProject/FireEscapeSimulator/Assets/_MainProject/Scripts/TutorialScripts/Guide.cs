1bac99964824d18415aa8733047219fda8899b5e
Carlier PE <Carlier.PE@gmail.com>
2025-06-26 09:23:19 +0200

    upgraded the guide class, picto class is there

--- UnityProject/FireEscapeSimulator/Assets/_MainProject/Scripts/TutorialScripts/Guide.cs
+++ UnityProject/FireEscapeSimulator/Assets/_MainProject/Scripts/TutorialScripts/Guide.cs
@@ -8,4 +8,38 @@ public class Guide : MonoBehaviour
      * and the path manager 
      * 
      */
+
+    [SerializeField] Transform _pictoContainer;
+    private TutorialPicto _currentPicto;
+
+    private void Update()
+    {
+        if (_currentPicto == null)
+            return;
+        _currentPicto.transform.position = _pictoContainer.position;
+        _currentPicto.transform.rotation = _pictoContainer.rotation;
+    }
+    public void Spawn(Vector3 spawnPosition, Transform lookAtTarget)
+    {
+        //se positionne à la position en regardant à lookattarget et joue son animation de spawn
+    }
+
+    public void ShowPanel(TutorialPicto picto)
+    {
+        //jouer l'animation du casque qui s'ouvre et montrer le picto
+        _currentPicto = picto;
+    }
+
+    public void HideCurrentPanel()
+    {
+        //jouer l'animation pour cacher le picto
+        _currentPicto = null;
+    }
+
+    //Just to have an idea of which animations we will have...
+    public void PlayGoForwardAnimation() { }
+    public void PlayGoBackwardAnimation() { }
+    public void PlayIdleAnimation() { }
+    public void PlayTurnLeftAnimation() { }
+    public void PlayTurnRightAnimation() { }
 }
