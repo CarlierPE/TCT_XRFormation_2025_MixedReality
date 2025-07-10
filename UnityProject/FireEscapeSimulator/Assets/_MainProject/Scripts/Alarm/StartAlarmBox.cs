9326f089ddd040c03b9f7e388b7b6512a88aaeb0
sebastien Verhaegen <sebastien.verhaegen@outlook.com>
2025-07-10 12:49:11 +0200

    Delete the debig log in thz script StartAlarm

--- UnityProject/FireEscapeSimulator/Assets/_MainProject/Scripts/Alarm/StartAlarmBox.cs
+++ UnityProject/FireEscapeSimulator/Assets/_MainProject/Scripts/Alarm/StartAlarmBox.cs
@@ -9,13 +9,7 @@ namespace TcT.FireSim
 
         public void StartSound()
         {
-            Debug.Log("entrer dans start sound");
-            if (_alarm == null)
-            {
-                Debug.Log("entrer dans le if car _alarm est null");
-                return;
-            }
-            Debug.Log("nombre d'element est " + _alarm.Count);
+            
             foreach (AudioSource source in _alarm)
             {
                 source.Play();
@@ -24,13 +18,7 @@ namespace TcT.FireSim
 
         public void StopSound()
         {
-            Debug.Log("entrer dans stop sound");
-            if (_alarm == null)
-            {
-                Debug.Log("entrer dans le if car _alarm est null");
-                return;
-            }
-            Debug.Log("nombre d'element est " + _alarm.Count);
+            
             foreach (AudioSource source in _alarm)
             {
                 source.Stop();
